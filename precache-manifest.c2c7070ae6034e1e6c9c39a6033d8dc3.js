3c3445245821ec315393fdc9ab136041b958398a
Patrik Torn <patrik.torn@outlook.com>
2019-09-10 10:34:46 +0300

    Updates

---
+++ precache-manifest.c2c7070ae6034e1e6c9c39a6033d8dc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ac6c384cbb3c8417ef53fd80929436",
+    "url": "/viikkis-client/static/media/viikkis.86ac6c38.png"
+  },
+  {
+    "revision": "cdf3648177a8c2ecc307",
+    "url": "/viikkis-client/static/css/main.589c5fb0.chunk.css"
+  },
+  {
+    "revision": "82107eaec137b05cd5a480867a508590",
+    "url": "/viikkis-client/static/media/iconfont.82107eae.svg"
+  },
+  {
+    "revision": "9259b8510807d881caee",
+    "url": "/viikkis-client/static/js/1.9259b851.chunk.js"
+  },
+  {
+    "revision": "811ba2db68d96dd318a9",
+    "url": "/viikkis-client/static/js/runtime~main.811ba2db.js"
+  },
+  {
+    "revision": "cdf3648177a8c2ecc307",
+    "url": "/viikkis-client/static/js/main.cdf36481.chunk.js"
+  },
+  {
+    "revision": "9141b0332bcf7eb587b4564013af24d7",
+    "url": "/viikkis-client/static/media/iconfont.9141b033.woff"
+  },
+  {
+    "revision": "2326a17b0719d583a2d9d54c16816c27",
+    "url": "/viikkis-client/static/media/iconfont.2326a17b.ttf"
+  },
+  {
+    "revision": "61c479ed36e3cd9b1490d2e6bf6adc9e",
+    "url": "/viikkis-client/static/media/iconfont.61c479ed.eot"
+  },
+  {
+    "revision": "9259b8510807d881caee",
+    "url": "/viikkis-client/static/css/1.6cff847d.chunk.css"
+  },
+  {
+    "revision": "316ec28acce5a5496786280c8487156b",
+    "url": "/viikkis-client/index.html"
+  }
+];
